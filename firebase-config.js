51c05affb2fd03499481fbd52879a3288305a3d9
KING LASIYA <99592205+huththoooo@users.noreply.github.com>
2025-05-27 12:25:50 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,24 @@
-// Your Firebase configuration here
+// Import Firebase core and database
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
+// Firebase config (your original config)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyDbOqIJebNyev-eRXI2A9N9_vYEPXBY5YY",
+  authDomain: "urlweb-2d2fd.firebaseapp.com",
+  databaseURL: "https://urlweb-2d2fd-default-rtdb.firebaseio.com",
+  projectId: "urlweb-2d2fd",
+  storageBucket: "urlweb-2d2fd.appspot.com",
+  messagingSenderId: "479423517882",
+  appId: "1:479423517882:web:0e7744c369faedb0b39e83",
+  measurementId: "G-NC5H9TQBR0"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Get Realtime Database
 const database = getDatabase(app);
 
+// Export the database to use in other files
 export { database };
